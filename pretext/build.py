e82a710b50b5a5f53b697a79e5551c5953e2f1fd
Oscar Levin <oscar.levin@unco.edu>
2020-06-05 14:34:03 -0600

    copy premade images in source to output

--- pretext/build.py
+++ pretext/build.py
@@ -5,26 +5,43 @@ from . import static, document, utils
 
 
 def html(ptxfile,output,stringparams):
+    import os, shutil
     # from pathlib import Path
     # ptxfile = os.path.abspath('source/main.ptx')
     xslfile = static.filepath('pretext-html.xsl')
     # create output directories and move there.
     # output = os.path.abspath(output)
     utils.ensure_directory(output)
-    with utils.working_directory(output):
-        utils.ensure_directory('knowl')
-        utils.ensure_directory('images')
+    utils.ensure_directory(os.path.join(output,'knowl'))
+    utils.ensure_directory(os.path.join(output,'images'))
+    # Copy images from source/images
+    src = os.path.join(os.path.dirname(os.path.abspath(ptxfile)), 'images')
+    src_files = os.listdir(src)
+    for file_name in src_files:
+        full_file_name = os.path.join(src, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, os.path.join(output,'images'))
     # transform ptx using xsl:
     xsltproc(xslfile, ptxfile, outfile=None, outdir=output, stringparams=stringparams)
 
 
 def latex(ptxfile,output,stringparams):
+    import os
+    import shutil
     # import sys
     # ptxfile = os.path.abspath('source/main.ptx')
     xslfile = static.filepath('pretext-latex.xsl')
     #create output directory
     # output = os.path.abspath(output)
     utils.ensure_directory(output)
+    utils.ensure_directory(os.path.join(output, 'images'))
+    # Copy images from source/images
+    src = os.path.join(os.path.dirname(os.path.abspath(ptxfile)), 'images')
+    src_files = os.listdir(src)
+    for file_name in src_files:
+        full_file_name = os.path.join(src, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, os.path.join(output, 'images'))
     # Do the xsltproc equivalent:
     # params = {"latex.font.size": "'20pt'"}
     xsltproc(xslfile, ptxfile, outfile='main.tex', outdir=output, stringparams=stringparams)
@@ -46,10 +63,13 @@ def xsltproc(xslfile, xmlfile, outfile=None, outdir=".", stringparams={}):
     print('Transform the source')
     with utils.working_directory(outdir):
         newdom = transform(dom, **stringparams)
+        #grab the format from xslfile and use it to name logfile:
         logfile = xslfile[xslfile.find('pretext-'):].replace('pretext-','').replace('.xsl','')+'-build.log'
-        print(transform.error_log)
         with open(logfile,"w") as log:
             log.write(str(transform.error_log))
+        # also print error_log to console.
+        print(transform.error_log)
+        # Write output if not done by exsl:document:
         if outfile:
             print('writing output to file specified')
             with open(outfile, "w") as fh:
