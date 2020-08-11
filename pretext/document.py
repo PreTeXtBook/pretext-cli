bcd6b0c40422817651be5d122305101ef50e06b6
Steven Clontz <steven.clontz@gmail.com>
2020-08-11 21:10:28 +0000

    more elegant handling of namespaces, closes #49

--- pretext/document.py
+++ pretext/document.py
@@ -1,17 +1,27 @@
 from lxml import etree as ET
 from slugify import slugify
 
-XML_ID = "{http://www.w3.org/XML/1998/namespace}id"
+# Info on namespaces: http://lxml.de/tutorial.html#namespaces
+NSMAP = {
+    "xi": "http://www.w3.org/2001/XInclude",
+    "xml": "http://www.w3.org/XML/1998/namespace",
+}
+
+def nstag(prefix,suffix):
+    return "{" + NSMAP[prefix] + "}" + suffix
 
 def new(title_string):
-    pretext = ET.Element("pretext")
+    pretext = ET.Element(
+        "pretext",
+        nsmap=NSMAP
+    )
     docinfo = ET.SubElement(pretext,"docinfo")
     macros = ET.SubElement(docinfo,"macros")
     macros.text = "\\newcommand{\\foo}{bar}"
     lip = ET.SubElement(docinfo,"latex-image-preamble")
     lip.text = "\\usepackage{tikz}"
     doc = ET.SubElement(pretext, "book")
-    doc.set(XML_ID,slugify(title_string))
+    doc.set(nstag("xml","id"),slugify(title_string))
     title = ET.SubElement(doc,"title")
     title.text = title_string
     return ET.ElementTree(pretext)
@@ -19,7 +29,7 @@ def new(title_string):
 def add_chapter(pretext,title_string):
     book = pretext.xpath("//book")[0]
     chapter = ET.SubElement(book,"chapter")
-    chapter.set(XML_ID,slugify(title_string))
+    chapter.set(nstag("xml","id"),slugify(title_string))
     title = ET.SubElement(chapter,"title")
     title.text = title_string
 
