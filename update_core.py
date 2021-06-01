8d1612beda2b8bebfa7bb460da0b976d9eb8e10b
Oscar Levin <oscar.levin@unco.edu>
2021-06-01 15:26:53 -0600

    alternative implimentation of pretext/pretext core functions

--- update_core.py
+++ update_core.py
@@ -17,9 +17,11 @@ with open("pretext/static/CORE_COMMIT","r") as commitfile:
 r = requests.get(f"https://github.com/rbeezer/mathbook/archive/{commit}.zip")
 archive = zipfile.ZipFile(io.BytesIO(r.content))
 archive.extractall("tmp")
-shutil.copyfile(f"tmp/mathbook-{commit}/pretext/pretext.py", "pretext/core.py")
+# shutil.copyfile(f"tmp/mathbook-{commit}/pretext/pretext.py", "pretext/core.py")
 shutil.rmtree("pretext/static/xsl", ignore_errors=True)
 shutil.copytree(f"tmp/mathbook-{commit}/xsl", "pretext/static/xsl")
+shutil.rmtree("pretext/static/pretext", ignore_errors=True)
+shutil.copytree(f"tmp/mathbook-{commit}/pretext", "pretext/static/pretext")
 shutil.rmtree("tmp")
 
 print("Copied rbeezer/mathbook Python scripts.")
