7b329a2790a012d42c082f8f7cc165f57d9ca5cf
Steven Clontz <steven.clontz@gmail.com>
2021-08-03 14:04:15 +0000

    tweak release builder

--- scripts/build_release.py
+++ scripts/build_release.py
@@ -1,5 +1,15 @@
-import subprocess
-import os
+import subprocess, os, shutil
+
+# grab verison number
+with open("pretext/static/VERSION") as f:
+    version = f.read().strip()
+
+print(f"Building package for version {version}.")
+
+print("Removing any old builds...")
+if os.path.isdir("dist"):
+    shutil.rmtree("dist")
+os.makedirs("dist")
 
 # ensure up-to-date "static" resources
 subprocess.run(["python", "scripts/update_core.py"])
@@ -8,5 +18,7 @@ subprocess.run(["python", "scripts/zip_templates.py"])
 subprocess.run(["python", "setup.py", "sdist", "bdist_wheel"])
 
 # prompt user to upload to PyPI
-print("If all is successful, and you have the PyPI token saved to `.pypitoken`:")
-print("python -m twine upload dist/* -u __token__ -p $(cat .pypitoken)")
+print("If all is successful, and you have the PyPI token saved to `.pypitoken`, run:")
+print()
+print(f"  python -m twine upload dist/* -u __token__ -p $(cat .pypitoken)")
+print()
