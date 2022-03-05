c79b75e688fa8150cf8b62eb27be37b269542d28
Alex Jordan <alex.jordan@pcc.edu>
2022-03-04 21:17:12 -0800

    Update utils.py
    make it not HTML specific

--- pretext/utils.py
+++ pretext/utils.py
@@ -203,7 +203,7 @@ def serve_forever(directory,access="private",port=8000):
                 looking_for_port = False
                 url = url_for_access(access,port)
                 log.info(f"Server started at {url}")
-                log.info(f"(In a web browser, you may visit {url} to view HTML output.)")
+                log.info(f"(In a web browser, you may visit {url} to view output.)")
                 log.info("Use [Ctrl]+[C] to halt the server.\n")
                 httpd.serve_forever()
         except OSError:
