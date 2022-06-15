0f04eb356664967e508337a994e009d2eff73e3e
Steven Clontz <steven.clontz@gmail.com>
2022-06-15 21:25:23 +0000

    remove unused slugify package, update click package major version, require 1.0.6+ pdfCropMargins

--- setup.py
+++ setup.py
@@ -16,13 +16,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     install_requires=[
        'lxml==4.*',
-       'python-slugify==6.*',
        'requests>=2.28,<3',
        'watchdog==2.*',
        'GitPython==3.*',
-       'click==7.*',
-       "pdfCropMargins==1.*",
-       "pypdf2==1.*",
+       'click==8.*',
+       "pdfCropMargins>=1.0.6,<2",
        "click-logging==1.*",
     ],
     extras_require={
