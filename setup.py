a16aa21b188ba359e37833908afde49d68792a8a
Steven Clontz <steven.clontz@gmail.com>
2022-06-15 21:27:32 +0000

    update license classifier, closes #32

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         "Operating System :: OS Independent",
     ],
     python_requires=f">=3.8.5",
