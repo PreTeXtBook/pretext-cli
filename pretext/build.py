54f6d0e60aed99a8b2a5298edb0b1f17cfca8dd4
Steven Clontz <steven.clontz@gmail.com>
2022-06-16 00:21:41 +0000

    factor out 'diagram' generation into separate functions

--- pretext/build.py
+++ pretext/build.py
@@ -36,70 +36,13 @@ def pdf(ptxfile,pub_file,output,stringparams,custom_xsl,pdf_method):
 
 # Function to build diagrams/images contained in source.
 def diagrams(ptxfile, pub_file, output, params, target_format, diagrams_format, xmlid_root, pdf_method):
-    # Dictionary of formats for images based on source and target
-    formats = {
-        'pdf': {'latex-image' : [], 'sageplot': ['pdf','png'], 'asymptote': ['pdf']},
-        'latex':  {'latex-image': [], 'sageplot': ['pdf','png'], 'asymptote': ['pdf']},
-        'html': {'latex-image' : ['svg'], 'sageplot': ['html','svg'], 'asymptote': ['html']}
-               }
-    # set format to all when appropriate
-    if diagrams_format == 'all':
-        formats[target_format] = {key: 'all' for key in formats[target_format]}
-    # We assume passed paths are absolute.
-    # set images directory
-    # parse source so we can check for image types.
-    source_xml = ET.parse(ptxfile)
-    source_xml.xinclude()
+    generate_latex_image(ptxfile, pub_file, output, params, target_format, diagrams_format, xmlid_root, pdf_method)
+    generate_sageplot(ptxfile, pub_file, output, params, target_format, diagrams_format, xmlid_root)
+    generate_asymptote(ptxfile, pub_file, output, params, target_format, diagrams_format, xmlid_root)
+    generate_interactive(ptxfile, pub_file, output, params, xmlid_root)
+    generate_youtube(ptxfile, pub_file, output, params, xmlid_root)
 
-    if len(source_xml.xpath("/pretext/*[not(docinfo)]//latex-image")) > 0 and len(formats[target_format]['latex-image']) > 0:
-        image_output = os.path.abspath(os.path.join(output, 'latex-image'))
-        os.makedirs(image_output, exist_ok=True)
-        log.info('Now generating latex-images\n\n')
-        # call pretext-core's latex image module:
-        with utils.working_directory("."):
-            for outformat in formats[target_format]['latex-image']:
-                core.latex_image_conversion(
-                    xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
-                    xmlid_root=xmlid_root, dest_dir=image_output, outformat=outformat, method=pdf_method)
-    if len(source_xml.xpath("/pretext/*[not(docinfo)]//sageplot")) > 0 and len(formats[target_format]['sageplot']) > 0:
-        image_output = os.path.abspath(os.path.join(output, 'sageplot'))
-        os.makedirs(image_output, exist_ok=True)
-        log.info('Now generating sageplot images\n\n')
-        with utils.working_directory("."):
-            for outformat in formats[target_format]['sageplot']:
-                core.sage_conversion(
-                    xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
-                    xmlid_root=xmlid_root, dest_dir=image_output, outformat=outformat)
-    if len(source_xml.xpath("/pretext/*[not(docinfo)]//asymptote")) > 0 and len(formats[target_format]['asymptote']) > 0:
-        image_output = os.path.abspath(
-            os.path.join(output, 'asymptote'))
-        os.makedirs(image_output, exist_ok=True)
-        log.info('Now generating asymptote images\n\n')
-        with utils.working_directory("."):
-            for outformat in formats[target_format]['asymptote']:
-                core.asymptote_conversion(
-                    xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
-                    xmlid_root=xmlid_root, dest_dir=image_output, outformat=outformat, method='server')
-    if len(source_xml.xpath("/pretext/*[not(docinfo)]//interactive[not(@preview)]"))> 0:
-        image_output = os.path.abspath(
-                    os.path.join(output, 'preview'))
-        os.makedirs(image_output, exist_ok=True)
-        log.info('Now generating preview images for interactives\n\n')
-        with utils.working_directory("."):
-            core.preview_images(
-                xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
-                xmlid_root=xmlid_root, dest_dir=image_output)
-    if len(source_xml.xpath("/pretext/*[not(docinfo)]//video[@youtube]")) > 0:
-        image_output = os.path.abspath(
-            os.path.join(output, 'youtube'))
-        os.makedirs(image_output, exist_ok=True)
-        log.info('Now generating youtube previews\n\n')
-        with utils.working_directory("."):
-            core.youtube_thumbnail(
-                xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
-                xmlid_root=xmlid_root, dest_dir=image_output)
-
-
+# generate webwork assets
 def webwork(ptxfile, pub_file, dest_dir, params):
     # Assume passed paths are absolute.
     # Set directory for WW representations.
@@ -110,3 +53,117 @@ def webwork(ptxfile, pub_file, dest_dir, params):
         core.webwork_to_xml(
             xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
             abort_early=True, dest_dir=dest_dir, server_params=None)
+
+# generate latex-image assets
+def generate_latex_image(ptxfile, pub_file, output, params, target_format, diagrams_format, xmlid_root, pdf_method):
+    # Dictionary of formats for images based on target
+    formats = {
+        'pdf': None,
+        'latex':  None,
+        'html': ['svg'],
+    }
+    # set overwrite formats to all when appropriate
+    if diagrams_format == 'all':
+        formats[target_format] = {key: ['all'] for key in formats[target_format]}
+    # We assume passed paths are absolute.
+    # set images directory
+    # parse source so we can check for image types.
+    source_xml = ET.parse(ptxfile)
+    source_xml.xinclude()
+    if len(source_xml.xpath("/pretext/*[not(docinfo)]//latex-image")) > 0 and formats[target_format] is not None:
+        image_output = os.path.abspath(os.path.join(output, 'latex-image'))
+        os.makedirs(image_output, exist_ok=True)
+        log.info('Now generating latex-images\n\n')
+        # call pretext-core's latex image module:
+        with utils.working_directory("."):
+            for outformat in formats[target_format]:
+                core.latex_image_conversion(
+                    xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
+                    xmlid_root=xmlid_root, dest_dir=image_output, outformat=outformat, method=pdf_method)
+
+# generate sageplot assets
+def generate_sageplot(ptxfile, pub_file, output, params, target_format, diagrams_format, xmlid_root):
+    # Dictionary of formats for images based on target
+    formats = {
+        'pdf': ['pdf','png'],
+        'latex':  ['pdf','png'],
+        'html': ['html','svg'],
+    }
+    # set overwrite formats to all when appropriate
+    if diagrams_format == 'all':
+        formats[target_format] = {key: ['all'] for key in formats[target_format]}
+    # We assume passed paths are absolute.
+    # set images directory
+    # parse source so we can check for image types.
+    source_xml = ET.parse(ptxfile)
+    source_xml.xinclude()
+    if len(source_xml.xpath("/pretext/*[not(docinfo)]//sageplot")) > 0 and formats[target_format] is not None:
+        image_output = os.path.abspath(os.path.join(output, 'sageplot'))
+        os.makedirs(image_output, exist_ok=True)
+        log.info('Now generating sageplot images\n\n')
+        with utils.working_directory("."):
+            for outformat in formats[target_format]:
+                core.sage_conversion(
+                    xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
+                    xmlid_root=xmlid_root, dest_dir=image_output, outformat=outformat)
+
+# generate asymptote assets
+def generate_asymptote(ptxfile, pub_file, output, params, target_format, diagrams_format, xmlid_root):
+    # Dictionary of formats for images based on target
+    formats = {
+        'pdf': ['pdf'],
+        'latex':  ['pdf'],
+        'html': ['html'],
+    }
+    # set overwrite formats to all when appropriate
+    if diagrams_format == 'all':
+        formats[target_format] = {key: ['all'] for key in formats[target_format]}
+    # We assume passed paths are absolute.
+    # set images directory
+    # parse source so we can check for image types.
+    source_xml = ET.parse(ptxfile)
+    source_xml.xinclude()
+    if len(source_xml.xpath("/pretext/*[not(docinfo)]//asymptote")) > 0 and formats[target_format] is not None:
+        image_output = os.path.abspath(
+            os.path.join(output, 'asymptote'))
+        os.makedirs(image_output, exist_ok=True)
+        log.info('Now generating asymptote images\n\n')
+        with utils.working_directory("."):
+            for outformat in formats[target_format]:
+                core.asymptote_conversion(
+                    xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
+                    xmlid_root=xmlid_root, dest_dir=image_output, outformat=outformat, method='server')
+
+# generate interactive preview assets
+def generate_interactive(ptxfile, pub_file, output, params, xmlid_root):
+    # We assume passed paths are absolute.
+    # set images directory
+    # parse source so we can check for image types.
+    source_xml = ET.parse(ptxfile)
+    source_xml.xinclude()
+    if len(source_xml.xpath("/pretext/*[not(docinfo)]//interactive")) > 0:
+        image_output = os.path.abspath(
+                    os.path.join(output, 'preview'))
+        os.makedirs(image_output, exist_ok=True)
+        log.info('Now generating preview images for interactives\n\n')
+        with utils.working_directory("."):
+            core.preview_images(
+                xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
+                xmlid_root=xmlid_root, dest_dir=image_output)
+
+# generate youtube thumbnail assets
+def generate_youtube(ptxfile, pub_file, output, params, xmlid_root):
+    # We assume passed paths are absolute.
+    # set images directory
+    # parse source so we can check for image types.
+    source_xml = ET.parse(ptxfile)
+    source_xml.xinclude()
+    if len(source_xml.xpath("/pretext/*[not(docinfo)]//video[@youtube]")) > 0:
+        image_output = os.path.abspath(
+            os.path.join(output, 'youtube'))
+        os.makedirs(image_output, exist_ok=True)
+        log.info('Now generating youtube previews\n\n')
+        with utils.working_directory("."):
+            core.youtube_thumbnail(
+                xml_source=ptxfile, pub_file=utils.linux_path(pub_file), stringparams=params,
+                xmlid_root=xmlid_root, dest_dir=image_output)
