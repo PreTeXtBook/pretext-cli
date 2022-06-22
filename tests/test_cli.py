b5225d643c5ef4cdb60f506d4e118b538f8ff986
Steven Clontz <steven.clontz@gmail.com>
2022-06-22 23:55:12 +0000

    add entry points test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,9 +5,16 @@ import pretext
 
 EXAMPLES_DIR = Path(__file__).parent/'ptx'
 
-def pretext_new_cd():
+def pretext_new_cd() -> None:
     subprocess.run(["pretext","new"])
     os.chdir(Path("new-pretext-project"))
+
+def cmd_works(*args) -> bool:
+    return subprocess.run(args).returncode == 0
+
+def test_entry_points():
+    assert cmd_works('pretext', '-h')
+    assert cmd_works('python', '-m', 'pretext', '-h')
 
 def test_version():
     cp = subprocess.run(['pretext', '--version'], capture_output=True, text=True)
@@ -15,7 +22,7 @@ def test_version():
 
 def test_new(tmp_path:Path):
     os.chdir(tmp_path)
-    assert subprocess.run(['pretext', 'new', '-d', 'foobar']).returncode == 0
+    assert cmd_works('pretext', 'new', '-d', 'foobar')
     assert (Path('foobar')/'project.ptx').exists()
 
 def test_build(tmp_path:Path):
@@ -33,14 +40,14 @@ def test_init(tmp_path:Path):
     refresh = Path('refresh').resolve()
     refresh.mkdir()
     os.chdir(fresh)
-    assert subprocess.run(['pretext', 'init']).returncode == 0
+    assert cmd_works('pretext', 'init')
     assert Path('project.ptx').exists()
     assert Path('requirements.txt').exists()
     assert Path('.gitignore').exists()
     assert Path('publication/publication.ptx').exists()
     os.chdir(refresh)
     pretext_new_cd()
-    assert subprocess.run(['pretext', 'init']).returncode == 0
+    assert cmd_works('pretext', 'init')
     assert len(glob.glob('project-*.ptx')) == 0 # need to refresh
     assert subprocess.run(['pretext', 'init', '-r']).returncode == 0
     assert len(glob.glob('project-*.ptx')) > 0 # need to refresh
@@ -53,5 +60,5 @@ def test_generate(tmp_path:Path):
     subprocess.run(['pretext', 'init'])
     Path('source').mkdir()
     shutil.copyfile(EXAMPLES_DIR/'asymptote.ptx',Path('source')/'main.ptx')
-    assert subprocess.run(['pretext','generate','web','-a','asymptote']).returncode == 0
+    assert cmd_works('pretext','generate','web','-a','asymptote')
     assert (Path('generated-assets')/'asymptote'/'test.html').exists()
