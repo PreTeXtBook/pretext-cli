78cac9eabc5e9c6d35e3ff86519e6c19a53da832
Oscar Levin <oscar.levin@unco.edu>
2022-06-26 16:14:36 -0600

    update core commit; remove pdf-crop-margins from templates

--- pretext/__init__.py
+++ pretext/__init__.py
@@ -17,4 +17,4 @@ from pathlib import Path
 from single_version import get_version
 VERSION = get_version('pretextbook', Path(__file__).parent.parent)
 
-CORE_COMMIT = "7fd12cdfb6d16c522b04f74866aa3aea01685106"
+CORE_COMMIT = "df98425042039eb3e809a34e38c010506d71d1bc"
