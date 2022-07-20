c3a3138005444cf567ed5874451f8449f4c29875
Steven Clontz <steven.clontz@gmail.com>
2022-07-20 00:16:14 +0000

    fix build_package to use fetch_core

--- scripts/build_package.py
+++ scripts/build_package.py
@@ -1,11 +1,11 @@
 import subprocess, pretext
-import update_core, zip_templates
+import fetch_core, zip_templates
 
 def main():
     print(f"Building package for version {pretext.VERSION}.")
 
     # ensure up-to-date "static" resources
-    update_core.main()
+    fetch_core.main()
     zip_templates.main()
 
     # Build package
