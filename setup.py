4ce7d67df226d2a6ba294bca5d1a69c996181248
Steven Clontz <steven.clontz@gmail.com>
2022-08-14 18:10:24 +0000

    annotate fake setup.py files

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# We use poetry and pyproject.toml, not setup.py.
+# To capture projects on GitHub using such versions we fake a setup.py here.
+
 raise Exception("File provided to satisfy GitHub only.")
 
 setup(name="pretext")
