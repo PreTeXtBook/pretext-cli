a8460a224bc949f3cbcb954599fbf73076bc53d1
Steven Clontz <steven.clontz@gmail.com>
2022-12-20 17:04:05 +0000

    add test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,3 +23,17 @@ def test_project_path(tmp_path: Path):
     subdir.mkdir()
     os.chdir(subdir)
     assert utils.project_path().resolve() == Path().resolve().parent
+
+
+def test_cached_project_path(tmp_path: Path):
+    os.chdir(tmp_path)
+    subdir1 = tmp_path / "foobar1"
+    subdir1.mkdir()
+    subdir2 = tmp_path / "foobar2"
+    subdir2.mkdir()
+    os.chdir(subdir1)
+    Path("project.ptx").write_text("")
+    assert Path("project.ptx").exists()
+    assert utils.project_path().resolve() == subdir1.resolve()
+    with utils.working_directory(subdir2):  # project should be cached as subdir1
+        assert utils.project_path() == subdir1.resolve()
