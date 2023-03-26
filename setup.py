ea7ab6c020935a9226981e70fe045ab1f0f30d17
Steven Clontz <steven.clontz@gmail.com>
2023-03-26 18:57:27 -0500

    Remove error from setup.py to try to address GH dependency issues

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 # We use poetry and pyproject.toml, not setup.py.
 # To capture projects on GitHub using such versions we fake a setup.py here.
 
-raise Exception("File provided to satisfy GitHub only.")
-
 setup(name="pretext")
