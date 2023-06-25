c0c9492e68cdd3aa9d131cac8c0f49ee2d5da193
Steven Clontz <steven.clontz@gmail.com>
2023-06-25 22:16:07 +0000

    poetry tweak plus install playwright upfront

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,14 +3,20 @@
 echo "Install Poetry and Python dependencies"
 curl -sSL https://install.python-poetry.org | python -
 poetry install
+poetry config virtualenvs.in-project true
 poetry run python scripts/fetch_core.py
 poetry run python scripts/zip_templates.py
+poetry run playwright install-deps
+poetry run playwright install
+
+echo "Update apt"
+sudo apt update
 
 echo "Install LaTeX"
-apt update && apt install -y texlive texlive-latex-extra texlive-fonts-extra texlive-xetex texlive-science --no-install-recommends
+sudo apt install -y texlive texlive-latex-extra texlive-fonts-extra texlive-xetex texlive-science --no-install-recommends
 
 echo "Install sage"
-apt install -y sagemath --no-install-recommends
+sudo apt install -y sagemath --no-install-recommends
 
 echo "Install PDF tools"
-apt install -y ghostscript pdf2svg --no-install-recommends
+sudo apt install -y ghostscript pdf2svg --no-install-recommends
