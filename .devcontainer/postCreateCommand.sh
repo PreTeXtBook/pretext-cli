63f950700e5411d815449d8110e59e7fe6a0cbd9
Steven Clontz <steven.clontz@gmail.com>
2023-06-26 23:27:31 +0000

    use full pretext image for dev

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,21 +2,11 @@
 
 echo "Install Poetry and Python dependencies"
 curl -sSL https://install.python-poetry.org | python -
-poetry config virtualenvs.in-project true
+echo 'export PATH="/root/.local/bin:$PATH"' > ~/.bashrc
+. ~/.bashrc
+poetry config virtualenvs.create false
 poetry install --with dev
-poetry run python scripts/fetch_core.py
-poetry run python scripts/zip_templates.py
-poetry run playwright install-deps
-poetry run playwright install
-
-echo "Update apt"
-sudo apt update
-
-echo "Install LaTeX"
-sudo apt install -y texlive texlive-latex-extra texlive-fonts-extra texlive-xetex texlive-science --no-install-recommends
-
-echo "Install sage"
-sudo apt install -y sagemath --no-install-recommends
-
-echo "Install PDF tools"
-sudo apt install -y ghostscript pdf2svg --no-install-recommends
+python scripts/fetch_core.py
+python scripts/zip_templates.py
+playwright install-deps
+playwright install
