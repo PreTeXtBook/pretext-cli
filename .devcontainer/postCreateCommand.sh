7d1febeb5c158622c5b311ff58479a4f292b82ff
bjones1 <bjones@ece.msstate.edu>
2023-10-24 16:25:55 -0500

    Fix: permissions problems for Poetry install.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,7 +5,8 @@ curl -sSL https://install.python-poetry.org | python -
 echo 'export PATH="/root/.local/bin:$PATH"' > ~/.bashrc
 . ~/.bashrc
 poetry config virtualenvs.create false
-poetry install --with dev
+# This invocation requires root access, but Poetry isn't in the path.
+sudo `which poetry` install --with dev
 python scripts/fetch_core.py
 python scripts/zip_templates.py
 playwright install-deps
