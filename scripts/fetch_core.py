55a1ec8a24d985423f8bbf1429681e08e27e2f36
Oscar Levin <oscar.levin@unco.edu>
2024-01-23 14:21:15 -0700

    add js resrouces (#669)

--- scripts/fetch_core.py
+++ scripts/fetch_core.py
@@ -21,7 +21,7 @@ def main() -> None:
         archive.extractall(tmpdirname)
         print("Creating zip of static folders")
         # Copy required folders to a single folder to be zipped:
-        for subdir in ["xsl", "schema", "script", "css", "pretext"]:
+        for subdir in ["xsl", "schema", "script", "css", "js", "js_lib", "pretext"]:
             shutil.copytree(
                 Path(tmpdirname) / f"pretext-{CORE_COMMIT}" / subdir,
                 Path(tmpdirname) / "static" / subdir,
