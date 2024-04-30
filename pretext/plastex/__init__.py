53e283ed8663b56c6832b922663a74eb4732a388
Oscar Levin <oscar.levin@unco.edu>
2024-04-30 02:20:27 +0000

    allow input/includes

--- pretext/plastex/__init__.py
+++ pretext/plastex/__init__.py
@@ -17,15 +17,30 @@ Renderer = Pretext
 def convert(input_file: Path, output: Path):
     log.info(f'Converting {input_file} to {output}')
 
-    with open(input_file, 'r') as f:
-        tex = TeX()
-        # tex.ownerDocument.config['files']['split-level'] = -100
-        # tex.ownerDocument.config['files']['filename'] = "test.xml"
-        tex.input(f.read())
-        doc = tex.parse()
+    def getLines(input_file: Path):
+        with open(input_file, 'r') as f:
+            lines = str()
+            line = f.readline()
+            print(line)
+            while line:
+                if line.strip().startswith("\\input") or line.strip().startswith("\\include"):
+                    inner_file = input_file.parent / line[line.find("{")+1:line.find("}")]
+                    lines = lines + "\n" + getLines(inner_file) + "\n"
+                else:
+                    lines += line
+                line = f.readline()
+            return lines
 
-        renderer = Renderer()
-        renderer.render(doc)
+    tex = TeX()
+    tex.input(getLines(input_file))
+    doc = tex.parse()
+
+    # tex.ownerDocument.config['files']['split-level'] = -100
+    # tex.ownerDocument.config['files']['filename'] = "test.xml"
+    # tex.input(f.read())
+
+    renderer = Renderer()
+    renderer.render(doc)
 
     # shutil.move('test.xml', output)
     log.info('\nConversion complete')
