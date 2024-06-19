9d149a70531d0da8db5170589098839f13cedeea
Steven Clontz <steven.clontz@gmail.com>
2024-06-19 23:18:41 +0000

    type ignore

--- pretext/cli.py
+++ pretext/cli.py
@@ -186,7 +186,7 @@ def main(ctx: click.Context, targets: bool) -> None:
 
 
 @main.result_callback()
-def exit_with_errors(*_, **__) -> None:
+def exit_with_errors(*_, **__):  # type: ignore
     if error_exit_handler.fired:
         raise SystemExit(1)
 
