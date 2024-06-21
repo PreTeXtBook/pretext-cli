195b68cfed297a0dc465889dbe9add8bfc2029fb
Steven Clontz <steven.clontz@gmail.com>
2024-06-21 18:11:22 +0000

    format

--- tests/test_sample_article.py
+++ tests/test_sample_article.py
@@ -14,9 +14,7 @@ from .common import check_installed, EXAMPLES_DIR
 def test_sample_article(tmp_path: Path) -> None:
     error_checker = errorhandler.ErrorHandler(logger="ptxlogger")
     prj_path = tmp_path / "sample"
-    shutil.copytree(
-        EXAMPLES_DIR / "core" / "examples" / "sample-article", prj_path
-    )
+    shutil.copytree(EXAMPLES_DIR / "core" / "examples" / "sample-article", prj_path)
     with pretext.utils.working_directory(prj_path):
         project = Project.parse()
         t = project.get_target()
