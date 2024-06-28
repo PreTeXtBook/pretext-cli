ad3e97e1484c825754a2c5790d31fa84b319ffa4
Oscar Levin <oscar.levin@unco.edu>
2024-06-28 15:02:49 -0600

    fix up dev scripts

---
+++ scripts/unlink_core.py
@@ -0,0 +1,19 @@
+import shutil
+from pathlib import Path
+import pretext.resources
+from scripts import fetch_core, utils
+
+
+# Restore the original core directory and reinstall core resources into home/.ptx appropriately
+def main() -> None:
+    link_path = pretext.resources.resource_base_path() / "core"
+    utils.remove_path(link_path)
+
+    # fetch_core.main()
+    print("Restored original core directory and reinstalled core resources.")
+    pretext.resources.install(reinstall=True)
+    print("Finished unlinking core resources.")
+
+
+if __name__ == "__main__":
+    main()
