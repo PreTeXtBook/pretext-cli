d978a7024dc497407e67264572bd4930c1297c09
Oscar Levin <oscar.levin@unco.edu>
2025-01-29 14:15:07 -0700

    set up for rs_services caching; clean up build scripts

--- scripts/fetch_core.py
+++ scripts/fetch_core.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import subprocess
 import requests
 import shutil
 import tempfile
@@ -8,14 +7,47 @@ import zipfile
 
 from pretext import CORE_COMMIT
 import utils
+import lxml.etree as ET  # noqa: N812
 
 import bundle_resources
 
 
-def update_css(ptx_dir) -> None:
-    print(f"current working directory: {os.getcwd()}")
-    with utils.working_directory(ptx_dir / "script" / "cssbuilder"):
-        print(f"current working directory: {os.getcwd()}")
+def get_runestone_services() -> None:
+    # work in temporary directory:
+    with tempfile.TemporaryDirectory(prefix="ptxcli_") as tmpdirname:
+
+        # download runestone files to temporary directory:
+
+        # Get runestone services file:
+        rs_services_path = Path(tmpdirname) / "runestone_services.xml"
+        rs_services_file = requests.get(
+            "https://runestone.academy/cdn/runestone/latest/webpack_static_imports.xml",
+            timeout=10,
+        )
+
+        with open(rs_services_path, "wb") as f:
+            f.write(rs_services_file.content)
+            services = ET.fromstring(rs_services_file.content)
+            # Interrogate the services XML
+            rs_cdn_url = services.xpath("/all/cdn-url")[0].text
+            # single Runestone Services version
+            rs_version = services.xpath("/all/version")[0].text
+
+        # Get the rs_services tgz file
+        services_file_name = "dist-{}.tgz".format(rs_version)
+        rs_services_tgz_path = Path(tmpdirname) / services_file_name
+        rs_services_tgz = requests.get(rs_cdn_url + services_file_name, timeout=10)
+        with open(rs_services_tgz_path, "wb") as f:
+            f.write(rs_services_tgz.content)
+
+        # Now zip these two files and put them in the resources directory
+        with zipfile.ZipFile(
+            Path("pretext").resolve() / "resources" / "rs_cache.zip",
+            "w",
+            zipfile.ZIP_DEFLATED,
+        ) as zip_ref:
+            zip_ref.write(rs_services_path, arcname="runestone_services.xml")
+            zip_ref.write(rs_services_tgz_path, arcname=services_file_name)
 
 
 def main() -> None:
@@ -23,41 +55,29 @@ def main() -> None:
 
     print(f"Requesting core PreTeXtBook/pretext commit {CORE_COMMIT} from GitHub.")
     core_zip_path = Path("pretext").resolve() / "resources" / "core.zip"
-    r = requests.get(
-        f"https://github.com/PreTeXtBook/pretext/archive/{CORE_COMMIT}.zip"
+    core_zip = requests.get(
+        f"https://github.com/PreTeXtBook/pretext/archive/{CORE_COMMIT}.zip", timeout=20
     )
     # remove current core/pretext.py file in case it is a link
     utils.remove_path(Path("pretext").resolve() / "core" / "pretext.py")
 
     with open(core_zip_path, "wb") as f:
-        f.write(r.content)
+        f.write(core_zip.content)
+
     with tempfile.TemporaryDirectory(prefix="ptxcli_") as tmpdirname:
         with zipfile.ZipFile(core_zip_path) as archive:
             archive.extractall(tmpdirname)
-            # Run the cssbuilder script:
-            script_dir = (
-                Path(tmpdirname) / f"pretext-{CORE_COMMIT}" / "script" / "cssbuilder"
-            )
-            npm_command = shutil.which("npm")
-            print(f"npm command: {npm_command}")
-            subprocess.run(
-                [npm_command, "install"],
-                cwd=script_dir,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            subprocess.run(
-                [npm_command, "run", "build"],
-                cwd=script_dir,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            # remove all the node_modules we used to build; we don't need anything in cssbuilder.
-            shutil.rmtree(script_dir / "node_modules")
-            print("Successfully built CSS.")
+
             shutil.copyfile(
                 Path(tmpdirname) / f"pretext-{CORE_COMMIT}" / "pretext" / "pretext.py",
                 Path("pretext").resolve() / "core" / "pretext.py",
+            )
+            shutil.copyfile(
+                Path(tmpdirname)
+                / f"pretext-{CORE_COMMIT}"
+                / "pretext"
+                / "braille_format.py",
+                Path("pretext").resolve() / "core" / "braille_format.py",
             )
             shutil.copytree(
                 Path(tmpdirname) / f"pretext-{CORE_COMMIT}" / "examples",
@@ -90,6 +110,10 @@ def main() -> None:
                             file_path, arcname=os.path.relpath(file_path, tmpdirname)
                         )
     print("Successfully updated core PreTeXtBook/pretext resources from GitHub.")
+
+    # Get Runestone services file
+    get_runestone_services()
+
     bundle_resources.main()
     print("Successfully bundled core PreTeXtBook/pretext resources.")
 
