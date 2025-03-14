f386acfc57c16a6288affc9750c9f1ca31120715
Oscar Levin <oscar.levin@unco.edu>
2025-03-14 11:45:21 -0600

    make cli.version only report minor version

--- pretext/project/__init__.py
+++ pretext/project/__init__.py
@@ -608,8 +608,9 @@ class Target(pxml.BaseXmlModel, tag="target", search_mode=SearchMode.UNORDERED):
         if hasattr(core.get_publisher_variable, "variables"):
             delattr(core.get_publisher_variable, "variables")
 
-        # Add cli.version to stringparams
-        self.stringparams["cli.version"] = VERSION
+        # Add cli.version to stringparams.  Use only the major and minor version numbers.
+        self.stringparams["cli.version"] = VERSION[: VERSION.rfind(".")]
+        log.debug(f"Set cli.version to {self.stringparams["cli.version"]}")
 
         # Check for xml syntax errors and quit if xml invalid:
         if not utils.xml_syntax_is_valid(self.source_abspath()):
