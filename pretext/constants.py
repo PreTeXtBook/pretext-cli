f96b99260653e22a38e53c20d716af3cb05dc659
Oscar Levin <oscar.levin@unco.edu>
2025-03-14 12:38:23 -0600

    fix dynamic-subs bug

--- pretext/constants.py
+++ pretext/constants.py
@@ -26,6 +26,7 @@ ASSETS_BY_FORMAT = {
         "codelens",
         "datafile",
         "myopenmath",
+        "dynamic-subs",
     ],
     "pdf": [
         "webwork",
@@ -101,6 +102,7 @@ ASSETS_BY_FORMAT = {
         "codelens",
         "datafile",
         "myopenmath",
+        "dynamic-subs",
     ],
     "webwork": [
         "webwork",
