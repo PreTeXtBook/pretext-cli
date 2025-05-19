9e03cf802efe14e9f5c4f2c46ccc6b38c3287643
Oscar Levin <oscar.levin@unco.edu>
2025-05-19 09:38:57 -0600

    update course template

--- templates/installPretext.sh
+++ templates/installPretext.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This file was automatically generated with PreTeXt 2.18.1.
+# This file was automatically generated with PreTeXt 2.18.3.
 # If you modify this file, PreTeXt will no longer automatically update it.
 
 sudo apt-get update 
