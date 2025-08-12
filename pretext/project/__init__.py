54d809cc3fb59d517269482f858d402cc6c616db
Oscar Levin <oscar.levin@unco.edu>
2025-08-12 07:32:31 -0600

    add --slow option to generate command to increase preview timeout

--- pretext/project/__init__.py
+++ pretext/project/__init__.py
@@ -877,6 +877,7 @@ class Target(pxml.BaseXmlModel, tag="target", search_mode=SearchMode.UNORDERED):
         xmlid: t.Optional[str] = None,
         clean: bool = False,
         skip_cache: bool = False,
+        slow: bool = False,
     ) -> None:
         """
         Generates assets for the current target.  Options:
@@ -1145,6 +1146,7 @@ class Target(pxml.BaseXmlModel, tag="target", search_mode=SearchMode.UNORDERED):
                     stringparams=stringparams_copy,
                     xmlid_root=xmlid,
                     dest_dir=self.generated_dir_abspath() / "preview",
+                    method="slow" if slow else "fast"
                 )
                 successful_assets.append("interactive")
             except Exception as e:
