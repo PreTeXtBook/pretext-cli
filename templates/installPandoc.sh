0d3a018a1135e244978138ccd9ade2e7dad3cfd9
Oscar Levin <oscar.levin@unco.edu>
2025-12-30 20:49:57 -0700

    updates to devcontainer in templates

--- templates/installPandoc.sh
+++ templates/installPandoc.sh
@@ -3,7 +3,7 @@
 # This file was automatically generated with PreTeXt {VERSION}.
 # If you modify this file, PreTeXt will no longer automatically update it.
 
-wget https://github.com/jgm/pandoc/releases/download/3.6.4/pandoc-3.6.4-1-amd64.deb -O pandoc.deb
+wget https://github.com/jgm/pandoc/releases/download/3.8.3/pandoc-3.8.3-1-amd64.deb -O pandoc.deb
 
 # wait for 60 second and then double check that no other script is using apt-get:
 sleep 60
@@ -12,6 +12,6 @@ while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
     sleep 15
 done
 # Install pandoc
-sudo apt-get install -y --no-install-recommends ./pandoc.deb 
+apt-get install -y --no-install-recommends ./pandoc.deb 
 
 rm pandoc.deb
