85a9ee1a0c3ff013cabb36f5d6fe4ffa3f270c98
Oscar Levin <oscar.levin@unco.edu>
2026-01-08 11:19:25 -0700

    fix pandoc install script to respect architecture

--- templates/installPandoc.sh
+++ templates/installPandoc.sh
@@ -3,7 +3,14 @@
 # This file was automatically generated with PreTeXt {VERSION}.
 # If you modify this file, PreTeXt will no longer automatically update it.
 
-wget https://github.com/jgm/pandoc/releases/download/3.8.3/pandoc-3.8.3-1-amd64.deb -O pandoc.deb
+# Detect architecture and download appropriate Pandoc version
+if uname -m | grep -q "aarch64\|arm64"; then
+    # ARM architecture
+    wget https://github.com/jgm/pandoc/releases/download/3.8.3/pandoc-3.8.3-1-arm64.deb -O pandoc.deb
+else
+    # x86/amd64 architecture
+    wget https://github.com/jgm/pandoc/releases/download/3.8.3/pandoc-3.8.3-1-amd64.deb -O pandoc.deb
+fi
 
 # wait for 60 second and then double check that no other script is using apt-get:
 sleep 60
