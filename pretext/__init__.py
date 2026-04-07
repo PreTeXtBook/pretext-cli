b60a774d3cd42548ea89b27a5f78c4c9803c8661
Oscar Levin <oscar.levin@unco.edu>
2026-04-06 20:55:43 -0600

    prepare new patch release

--- pretext/__init__.py
+++ pretext/__init__.py
@@ -19,7 +19,7 @@ from single_version import get_version
 VERSION = get_version("pretext", Path(__file__).parent.parent)
 
 
-CORE_COMMIT = "80f77abc530ebb9338b3fdfc8b349fdc2a919d4b"
+CORE_COMMIT = "22c2cd36f996308ae16df2b77aa60ee9af5a8e69"
 
 
 def activate() -> None:
