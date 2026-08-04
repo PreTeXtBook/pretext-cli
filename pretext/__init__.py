0f28b9af54a847f6917981dae7d03b90e8927e9c
Oscar Levin <oscar.levin@gmail.com>
2026-08-03 19:35:01 -0600

    Update core (#1203)

--- pretext/__init__.py
+++ pretext/__init__.py
@@ -18,7 +18,7 @@ from single_version import get_version
 
 VERSION = get_version("pretext", Path(__file__).parent.parent)
 
-CORE_COMMIT = "ea9c025222639b69dd7bf87dd3b9a48b507b8418"
+CORE_COMMIT = "d3082856ff572a27ccd6bfe4d3aae1a1e7dc6cc1"
 
 
 def activate() -> None:
